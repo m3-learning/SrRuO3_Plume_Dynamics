e821c7975cff12dc71cbceaa8de1e80d241b4156
yig319 <yig319@lehigh.edu>
2025-11-07 17:03:50 -0500

    update build

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
-    name="sro_sto_plume",
-    version="0.1",
-    description="Repository for SRO_STO_Plume project",
-    author="Yichen Guo",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),  # Will include all packages under 'src'
-    install_requires=[],  # Add dependencies if needed, e.g., ['numpy']
-)
+# setup(
+#     name="sro_sto_plume",
+#     version="0.1",
+#     description="Repository for SRO_STO_Plume project",
+#     author="Yichen Guo",
+#     package_dir={"": "src"},
+#     packages=find_packages(where="src"),  # Will include all packages under 'src'
+#     install_requires=[],  # Add dependencies if needed, e.g., ['numpy']
+# )
+
+from setuptools import setup
+setup()
